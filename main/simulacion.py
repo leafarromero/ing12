06fe5e96d24971b873d7b4c011e10d2f4b9bafd2
Luis Ignacio Cingolani <luiscingo@gmail.com>
2017-07-12 18:50:34 -0300

    agregado scheduler y contexto

--- main/simulacion.py
+++ main/simulacion.py
@@ -6,17 +6,18 @@ Created on 12 jul. 2017
 class Simulacion:
     
     def __init__(self):
-        self.miLog = Log("log")
+        self.log = Log("log")
+        self.diaNumero = 0
+        self.scheduler = Scheduler()
+        self.contexto = Contexto()
         
-    
     def comenzar(self):
-        pass
+        self.log.comenzar()
     
     def simularDia(self):
-        pass
+        self.log.escribirLinea("Comienza el dia " + self.diaNumero)
+        self.scheduler.ejecutarPoliticas(self.contexto)
     
-    def contexto(self):
-        pass
     
 
 class Log:
@@ -34,3 +35,32 @@ class Log:
             file.write(texto + "\n")
             
         return texto + "\n"
+
+class Scheduler():
+    def __init__(self):
+        self.politicas = []
+        
+    def ejecutarPoliticas(self,contexto):
+        for politica in self.politicas:
+            politica.decidir(contexto)
+    
+    def agregarPolitica(self, politica):
+        self.politicas.append(politica)
+
+
+class Contexto():
+    pass    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
--- main/tests.py
+++ main/tests.py
@@ -6,26 +6,32 @@ Created on 12 jul. 2017
 import unittest
 import main.simulacion as simulacion
 
-class TestsSimulacion(unittest.TestCase):
+class TestsLog(unittest.TestCase):
     def test01(self):
-        miSim = simulacion.Simulacion()
-        log = miSim.miLog
+        log = simulacion.Log()
         lineaEscrita = log.comenzar()
         
         self.assertEqual(lineaEscrita, "Comienzo de simulacion\n")
         
     def test02(self):
-        miSim = simulacion.Simulacion()
-        log = miSim.miLog
+        log = simulacion.Log()
         log.comenzar()
         lineaEscrita = log.escribirLinea("lalalalalalal")
         self.assertEqual(lineaEscrita, "lalalalalalal\n")
         
     def test03(self):
-        miSim = simulacion.Simulacion()
-        log = miSim.miLog
+        log = simulacion.Log()
         log.comenzar()
         lineaEscrita = log.escribirLinea("lalalalalalal")
         lineaEscrita = log.escribirLinea("hello there")
         self.assertNotEqual(lineaEscrita, "lalalalalalal\n")
         self.assertEqual(lineaEscrita, "hello there\n")
+
+    def test04(self):
+        log = simulacion.Log()
+        log.comenzar()
+        log.escribirLinea("algo")
+        
+        
+        
+        
