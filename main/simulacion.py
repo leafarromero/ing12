b2eb7e223f66ba43e19aa165c596d485a0180060
Luis Ignacio Cingolani <luiscingo@gmail.com>
2017-07-12 17:24:59 -0300

    primer commit setteo de proyecto

---
+++ main/simulacion.py
@@ -0,0 +1,19 @@
+'''
+Created on 12 jul. 2017
+
+@author: Luis
+'''
+class Simulacion:
+    
+    def __init__(self):
+        pass
+    
+    def comenzar(self):
+        pass
+    
+    def simularDia(self):
+        pass
+    
+    def contexto(self):
+        pass
+    
