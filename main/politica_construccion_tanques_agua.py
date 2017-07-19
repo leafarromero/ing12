758bd4adcc2a325976a90e8ced5bdd6b023da968
Cristian <cvazquez@dc.uba.ar>
2017-07-19 13:08:09 -0300

    La politica de construccion de tanques de agua y gas usan un admin de tanques de agua o gas.

--- main/politica_construccion_tanques_agua.py
+++ main/politica_construccion_tanques_agua.py
@@ -5,7 +5,8 @@ from .politica_ejecucion import PoliticaEjecucion
 class PoliticaConstruccionTanquesAgua(PoliticaEjecucion):
 
     def __init__(self):
-        _constructor = Constructor()
+        self._constructor = Constructor()
 
-    def decidir(self):
-        pass
+    def decidir(self,admin_tanques_agua):
+        if admin_tanques_agua.estan_al_limite():
+            self._constructor.construir_tanque_agua(1,1,1)
--- main/politica_construccion_tanques_gas.py
+++ main/politica_construccion_tanques_gas.py
@@ -5,7 +5,8 @@ from .politica_ejecucion import PoliticaEjecucion
 class PoliticaConstruccionTanquesGas(PoliticaEjecucion):
 
     def __init__(self):
-        _constructor = Constructor()
+        self._constructor = Constructor()
 
-    def decidir(self):
-        pass
+    def decidir(self,admin_tanques_gas):
+        if admin_tanques.gas.estan_al_limite():
+            self._constructor.contruir_tanque_gas(1,1,1)
