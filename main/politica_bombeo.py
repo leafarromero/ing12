e06ef183a2184adbdf585027faf6e246f865ec2c
Cristian <cristianvazquez4@gmail.com>
2017-07-18 21:00:36 -0300

    Add politica_bombeo

---
+++ main/politica_bombeo.py
@@ -0,0 +1,10 @@
+from politica_ejecucion import PoliticaEjecucion
+from simulacion import Bombeador
+
+class PoliticaBombeo(PoliticaEjecucion):
+
+    def __init__(self):
+        _bombeador = Bombeador()
+
+    def decidir(self):
+        pass
