0a19227c70d550a16023ff68cb73b62fe92b0009
cristian <cvazquez@dc.uba.ar>
2017-07-19 23:28:05 -0300

    construirPlantaSeparadora recibe una variable litros. el constructor de Constructor recibe un log.

--- main/constructor.py
+++ main/constructor.py
@@ -1,25 +1,26 @@
 class Constructor:
-	def __init__(self,log,estructuras,confPath):
-		self.log = log
-		self.estructuras = estructuras
-		archivo = confPath + "estructuras.txt"
-		with open(archivo, "r") as file:
-			linea = file.readLine()
-			lineaParseada = linea.split(" ")
-			self.costoTanqueAguaPorLitro = lineaParseada[1]
-			self.costoTanqueGasPorLitro = lineaParseada[3]
-			self.costoPlantaPorLitro = lineaParseada[5]
+    def __init__(self, log, estructuras, confPath):
+        self.log = log
+        self.estructuras = estructuras
+        archivo = confPath + "estructuras.txt"
+        with open(archivo, "r") as file:
+            linea = file.readLine()
+            lineaParseada = linea.split(" ")
+            self.costoTanqueAguaPorLitro = lineaParseada[1]
+            self.costoTanqueGasPorLitro = lineaParseada[3]
+            self.costoPlantaPorLitro = lineaParseada[5]
 
-	def construirTanqueAgua(self,litros):
-		log.escribirLinea("comienzo a construir tanque de agua, litros: " + str(litros) + "\n")
-		log.gasto(self.costoTanqueAguaPorLitro*litros)
-		self.estructuras.construirTanqueAgua(litros,log)
+    def construirTanqueAgua(self, litros):
+        self.log.escribirLinea("comienzo a construir tanque de agua, litros: " + str(litros) + "\n")
+        self.log.gasto(self.costoTanqueAguaPorLitro * litros)
+        self.estructuras.construirTanqueAgua(litros, self.log)
 
-	def construirTanqueGas(self,litros):
-		log.escribirLinea("comienzo a construir tanque de gas, litros: " + str(litros) + "\n")
-		log.gasto(self.costoTanqueGasPorLitro*litros)
-		self.estructuras.construirTanqueGas(litros,log)
-	def construirPlantaSeparadora(self):
-		log.escribirLinea("comienzo a construir planta separadora, litros: " + str(litros) + "\n")
-		log.gasto(self.costoPlantaPorLitro*litros)
-		self.estructuras.construirPlantaSeparadora(litros,log)
+    def construirTanqueGas(self, litros):
+        self.log.escribirLinea("comienzo a construir tanque de gas, litros: " + str(litros) + "\n")
+        self.log.gasto(self.costoTanqueGasPorLitro * litros)
+        self.estructuras.construirTanqueGas(litros, self.log)
+
+    def construirPlantaSeparadora(self, litros):
+        self.log.escribirLinea("comienzo a construir planta separadora, litros: " + str(litros) + "\n")
+        self.log.gasto(self.costoPlantaPorLitro * litros)
+        self.estructuras.construirPlantaSeparadora(litros, self.log)
