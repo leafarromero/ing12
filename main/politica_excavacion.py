640c8df1d1ac624cd23836fad36055b0ae0f36b7
cristian <cvazquez@dc.uba.ar>
2017-07-20 16:44:47 -0300

    Politica excavacion creada pero no implementada

---
+++ main/politica_excavacion.py
@@ -0,0 +1,16 @@
+from main.politica_ejecucion import PoliticaEjecucion
+
+
+class PoliticaExcavacion(PoliticaEjecucion):
+
+    def decidir(self):
+        pass
+
+
+class UnaPoliticaExcavacion(PoliticaExcavacion):
+
+    def __init__(self):
+        pass
+
+    def decidir(self):
+        pass
