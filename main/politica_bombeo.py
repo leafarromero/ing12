25cbbdc75cf669c78a1ce2f7c49861320b94093a
cristian <cvazquez@dc.uba.ar>
2017-07-20 15:15:08 -0300

    Errores que detecto pycharm

--- main/politica_bombeo.py
+++ main/politica_bombeo.py
@@ -3,17 +3,13 @@ from simulacion import Bombeador, TanqueAgua
 
 class PoliticaBombeo(PoliticaEjecucion):
 
-    def decidir(self,contexto, bombeador):
+    def decidir(self,contexto, bombeador, comprador):
         pass
 
 class UnaPoliticaBombeo(PoliticaBombeo):
     def __init__(self,
                  volumen_maximo_reinyeccion,
                  presion_critica,
-                 tanques_agua,
-                 tanques_gas,
-                 plantas_separadoras,
-                 yacimiento,
                  log,
                  dilusion_critica,
                  ):
@@ -21,8 +17,9 @@ class UnaPoliticaBombeo(PoliticaBombeo):
         self._volumen_maximo_reinyeccion = volumen_maximo_reinyeccion
         self._presion_critica = presion_critica
         self._dilusion_critica = dilusion_critica
+        self._log = log
 
-    def decidir(self,contexto, bombeador):
+    def decidir(self,contexto, bombeador, comprador):
         # Chequear que el pozo no este en dilusion critica
         
         unYacimiento = contexto.yacimiento
@@ -32,23 +29,30 @@ class UnaPoliticaBombeo(PoliticaBombeo):
         # Chequear si algun pozo hay que reinyectar
         if self.hay_que_reinyectar(unYacimiento):
             # en caso afirmativo no se puede extraer
+            volumen_a_reinyectar = self._volumen_maximo_reinyeccion
 
             # Se reinyecta con agua comprada o  agua y gas almacenada en los tanques
-            if contexto.estructuras.litrosDeGasAlmacenado() > 0:
-                unYacimiento.reinyectarGas(cant_gas)
+            gas_almacenado = contexto.estructuras.litrosDeGasAlmacenado()
+            min1 = min(gas_almacenado, volumen_a_reinyectar)
+            bombeador.reinyectarGas(min1)
+            volumen_a_reinyectar -= min1
 
+            # Usar Agua almacenada
+            agua_almacenada = contexto.estructuras.litrosDeAguaAlmacenada()
+            min2 = min(agua_almacenada, volumen_a_reinyectar)
+            bombeador.reinyectarAgua(min2)
+            volumen_a_reinyectar -= min2
+            
+            # Comprar agua
+            comprador.compradorDeAgua(volumen_a_reinyectar)
+            bombeador.reinyectarAgua(volumen_a_reinyectar)
 
-            if contexto.estructuras.litrosDeAguaAlmacenada() > 0:
-                # Usar usa agua
-                unYacimiento.reinyectarAgua(cant_agua)
-            else:
-                # Comprar agua
-                pass
 
 
         else:
             # en caso negativo se extrae de los pozos
-            pass
+            pozos = contexto.extructuras.
+            bombeador.extraer()
 
     def hay_que_reinyectar(self,yacimiento):
         __reinyectar = False
@@ -57,13 +61,11 @@ class UnaPoliticaBombeo(PoliticaBombeo):
                 reinyectar = True
         return reinyectar
 
-    def tengo_agua_gas_almacenada(self,contexto):
-        for tanque_agua in contexto.estructuras.dame_tanques_agua():
-            if tanque_agua.litros() > 0:
-                return True
 
-        for tanque_gas in contexto.dame_tanques_gas():
-            if tanque_gas.litros() > 0:
-                return True
+class CompradorAgua:
 
-        return False
+    def __init__(self):
+        pass
+
+    def comprar_agua(self,cantidad):
+        pass
